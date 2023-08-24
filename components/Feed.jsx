290b090ffbc0e403d82ac1b1af3cbfb03ab5a136
akugone <dev@akumaw.com>
2023-08-24 18:02:43 +0200

    add feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+'use client';
+
+import { useState, useEffect } from 'react';
+import HintCard from '@components/HintCard';
+
+const HintCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map(post => (
+        <HintCard key={post._id} post={post} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+  console.log(searchText);
+
+  const handSearchChange = e => {
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='search you tips'
+          onChange={handSearchChange}
+          value={searchText}
+          className='search_input peer '
+        />
+      </form>
+      <HintCardList data={[]} handletagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
