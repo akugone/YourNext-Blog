b54ec2d87211e06ee536745699975cce13689e33
Akumaw <dev@akumaw.com>
2023-08-28 10:47:08 +0200

    fix post problem

--- app/api/hint/new/route.js
+++ app/api/hint/new/route.js
@@ -1,34 +1,11 @@
-// pages/api/hints.js
-
 import Hint from '@models/hint';
 import { connectToDB } from '@utils/database';
-import { getSession } from 'next-auth/react';
-import { NextResponse } from 'next/server';
 
 export const POST = async req => {
-  const { hint, tag } = await req.json();
-  const session = await getSession({ req });
-
-  const userId = session.user.id;
-  console.log('session', session);
-  console.log('hint', hint);
-  console.log('tag', tag);
-
-  if (session) {
-    // Signed in
-    console.log('Session', JSON.stringify(session, null, 2));
-  } else {
-    // Not Signed in
-    return NextResponse.json(401);
-  }
-
-  if (!hint || !tag) {
-    throw new Error('hint or tag is missing from the request body');
-  }
+  const { userId, hint, tag } = await req.json();
 
   try {
     await connectToDB();
-
     const newHint = new Hint({
       creator: userId,
       hint: hint,
@@ -36,7 +13,7 @@ export const POST = async req => {
     });
 
     await newHint.save();
-    // Todo NextResponse.json(401)
+
     return new Response(JSON.stringify(newHint), { status: 201 });
   } catch (error) {
     return new Response('Failed to create a new Hint', { status: 500 });
